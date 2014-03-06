e164834b870f11fcb09a7e3fdf9e299792fd34f2
Sebastian Sprenger <sprengr@outlook.com>
2014-03-06 20:23:17 +0100

    fixed tests

--- integrationTests/tables.js
+++ integrationTests/tables.js
@@ -7,19 +7,10 @@ require('mocha-as-promised')();
 describe('Tables', function() {
     describe('#getTables', function() {
 
-        it.skip('should return a array of tables for a given database', function() {
+        it('should return a array of tables for a given database', function() {
             return conn.connect('./integrationTests/db/db-config.json')
                 .then(function(knex) {
-                    return tables.getTables().should.eventually.be.eql([
-                        {
-                            table_name: 'heinzel',
-                            table_schema: 'public'
-                        },
-                        {
-                            table_name: 'occupation',
-                            table_schema: 'public'
-                        }
-                    ]);
+                    return tables.getTables().should.eventually.have.length(2);
                 });
         });
 
--- test/tables.js
+++ test/tables.js
@@ -8,12 +8,12 @@ require('mocha-as-promised')();
 
 describe('Tables', function() {
     describe('#getTables', function() {
-        //TODO: multiple tables (array)
+
         it('should return table with columns', function() {
             tables.getNamesAndSchema = function(){
                 var q = Q.defer();
                 q.resolve([{table_name: 'heinzel', table_schema: 'public'},
-                           /*{table_name: 'occupation', table_schema: 'public'}*/]);
+                           {table_name: 'occupation', table_schema: 'public'}]);
                 return q.promise;
             };
             columns.get = function(){
@@ -23,14 +23,22 @@ describe('Tables', function() {
             };
             console.log(tables.getTables()[0]);
             return tables.getTables().should.eventually.be.eql(
-                {
+                [{
                     table_name: 'heinzel',
                     table_schema: 'public',
                     columns: [
                         {column_name: 'name'},
                         {column_name: 'id'}
                     ]
-                }
+                },
+                {
+                    table_name: 'occupation',
+                    table_schema: 'public',
+                    columns: [
+                        {column_name: 'name'},
+                        {column_name: 'id'}
+                    ]
+                }]
             );
         });
 

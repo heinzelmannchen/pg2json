73933b6df8f133950c4d84cee483c19c1b81c56a
Sebastian Sprenger <sprengr@outlook.com>
2014-03-06 20:41:04 +0100

    merge problems

--- test/tables.js
+++ test/tables.js
@@ -8,12 +8,16 @@ require('mocha-as-promised')();
 
 describe('Tables', function() {
     describe('#getTables', function() {
-        //TODO: multiple tables (array)
         it('should return table with columns', function() {
             tables.getNamesAndSchema = function() {
                 var q = Q.defer();
-                q.resolve([{table_name: 'heinzel', table_schema: 'public'},
-                           /*{table_name: 'occupation', table_schema: 'public'}*/]);
+                q.resolve([{
+                    table_name: 'heinzel',
+                    table_schema: 'public'
+                }, {
+                    table_name: 'occupation',
+                    table_schema: 'public'
+                }]);
                 return q.promise;
             };
             columns.get = function() {
@@ -26,15 +30,23 @@ describe('Tables', function() {
                 return q.promise;
             };
             return tables.getTables().should.eventually.be.eql(
-                {
+                [{
                     table_name: 'heinzel',
                     table_schema: 'public',
-                    columns: [
-                        {column_name: 'name'},
-                        {column_name: 'id'}
-                    ]
-                }
-            );
+                    columns: [{
+                        column_name: 'name'
+                    }, {
+                        column_name: 'id'
+                    }]
+                }, {
+                    table_name: 'occupation',
+                    table_schema: 'public',
+                    columns: [{
+                        column_name: 'name'
+                    }, {
+                        column_name: 'id'
+                    }]
+                }]);
         });
 
     });

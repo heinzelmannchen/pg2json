dccee8af94d2289bbe2d403a2b1ce7b207b863ce
Sebastian Sprenger <sprengr@outlook.com>
2014-03-08 09:45:20 +0100

    add relation in integration, portable query for relations

--- integrationTests/db/create.js
+++ integrationTests/db/create.js
@@ -1,18 +1,5 @@
 require('../../lib/connection').connect('./integrationTests/db/db-config.json')
     .then(function(knex) {
-        knex.schema.hasTable('heinzel').then(function(exists) {
-            if (!exists) {
-                return knex.schema.createTable('heinzel', function(t) {
-                    t.increments('id').primary();
-                    t.string('name', 100);
-                    t.string('hat_color', 100);
-                    t.text('age');
-                });
-            }
-        }).catch(function() {
-            console.warn('heinzel already exists');
-        });
-
         knex.schema.hasTable('occupation').then(function(exists) {
             if (!exists) {
                 return knex.schema.createTable('occupation', function(t) {
@@ -21,6 +8,21 @@ require('../../lib/connection').connect('./integrationTests/db/db-config.json')
                 });
             }
         }).catch(function() {
+            console.warn('occupation already exists');
+        });
+
+        knex.schema.hasTable('heinzel').then(function(exists) {
+            if (!exists) {
+                return knex.schema.createTable('heinzel', function(t) {
+                    t.increments('id').primary();
+                    t.string('name', 100);
+                    t.string('hat_color', 100);
+                    t.text('age');
+                    t.integer('occupation_fk').references('id').inTable('occupation');
+                });
+            }
+        }).catch(function(err) {
+            console.warn(err);
             console.warn('heinzel already exists');
         });
     })

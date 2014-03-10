07d063a69d6ec36b02931c307fecd6c647fece8b
Sebastian Sprenger <sprengr@outlook.com>
2014-03-10 15:49:44 +0100

    init

---
+++ integrationTests/relations.js
@@ -0,0 +1,18 @@
+﻿var relations = require('../lib/relations'),
+    conn = require('../lib/connection'),
+    sinon = require('sinon');
+
+require('mocha-as-promised')();
+
+describe('Relations', function() {
+    describe('#get', function() {
+
+        it('should list two relations (\'occupation\', \'gender\') for table \'heinzel\'', function() {
+            return conn.connect('./integrationTests/db/db-config.json')
+                .then(function(knex) {
+                    return relations.get('heinzel').should.eventually.have.length(2);
+            });
+        });
+
+    });
+});

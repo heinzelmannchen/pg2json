eeb2690cd580c0143eb98a003e97343399b1311e
Sebastian Sprenger <sprengr@outlook.com>
2014-03-10 15:41:39 +0100

    easy way to drop all tables

---
+++ integrationTests/db/drop.js
@@ -0,0 +1,18 @@
+﻿require('../../lib/connection').connect('./integrationTests/db/db-config.json')
+    .then(function(knex) {
+        drop('heinzel');
+        drop('gender');
+        drop('occupation');
+    return;
+});
+
+
+function drop(tableName) {
+    knex.schema.hasTable(tableName).then(function(exists) {
+        if (exists) {
+            return knex.schema.dropTable(tableName);
+        }
+    }).catch(function(error) {
+        console.warn(tableName + ' couldn\'t be dropped: ' + error);
+    });
+}

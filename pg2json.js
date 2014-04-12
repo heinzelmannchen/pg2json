eb3a2547af2d1fe1dedb9073a5e1099d11248ac8
Sebastian Sprenger <sprengr@outlook.com>
2014-04-12 16:46:06 +0200

    cli cleanup, pg2json writes files (force flag still missing)

--- pg2json.js
+++ pg2json.js
@@ -1,16 +1,31 @@
 var connection = require('./lib/connection'),
     columns = require('./lib/columns'),
     tables = require('./lib/tables'),
+    relations = require('./lib/relations'),
+    fsUtil = require('heinzelmannchen-fs'),
+    Q = require('q'),
     me = module.exports;
 
 me.connect = function(config) {
     return connection.connect(config);
 };
 
-me.getTables = function() {
-    return tables.getTables();
+me.get = function(tableNames) {
+    return tables.get(tableNames);
+};
+
+me.getTableNames = function(){
+    return tables.getMetadata();
 };
 
 me.getColumns = function(tableName) {
     return columns.get(tableName);
+};
+
+me.getRelations = function(tableName) {
+    return relations.get(tableName);
+};
+
+me.write = function(path, content) {
+    return fsUtil.createFile(path, content);
 };
